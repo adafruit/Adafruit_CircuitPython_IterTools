a4c049b7d529bfd601c1707a3b22856b408982a7
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_itertools/adafruit_itertools_extras.py
+++ adafruit_itertools/adafruit_itertools_extras.py
@@ -41,7 +41,7 @@ Based on code from the offical Python documentation.
 
 import adafruit_itertools as it
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_Itertools.git"
 
 
